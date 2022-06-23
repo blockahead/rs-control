058045f1ddff08e45b300d67ede8b137e4f1c8e1
Hamada_Kwansei <hamadaka@kwansei.ac.jp>
2022-06-23 13:50:50 +0900

    Remove no_std feature.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
-#![no_std]
-
 pub trait System<T> {
-    fn dxdt(&self, x: &[T], u: &[T], dxdt: &mut [T]);
-    fn y(&self, x: &[T], u: &[T], y: &mut [T]);
+    fn dxdt(&self, x: &[T], u: &[T]) -> Vec<T>;
+    fn y(&self, x: &[T], u: &[T]) -> Vec<T>;
     fn ndims(&self) -> (usize, usize, usize);
 }
