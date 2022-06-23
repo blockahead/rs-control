b99b3b0acf2c116ef951990f5b60fb3dfb6ee879
Hamada_Kwansei <hamadaka@kwansei.ac.jp>
2022-06-23 14:15:47 +0900

    Add nalgebra into dependencies.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
+use nalgebra::DVector;
+
 pub trait System<T> {
-    fn dxdt(&self, x: &[T], u: &[T]) -> Vec<T>;
-    fn y(&self, x: &[T], u: &[T]) -> Vec<T>;
+    fn dxdt(&self, x: &DVector<T>, u: &DVector<T>) -> DVector<T>;
+    fn y(&self, x: &DVector<T>, u: &DVector<T>) -> DVector<T>;
     fn ndims(&self) -> (usize, usize, usize);
 }
